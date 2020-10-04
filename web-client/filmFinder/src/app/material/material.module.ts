4eac452b092137c4f0c0f5f5fa289157d480e304
Jatin Gupta <guptajatin@outlook.com>
2020-10-04 13:45:48 +1100

    material module set up

---
+++ web-client/filmFinder/src/app/material/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+
+const MaterialComponents = [
+  MatButtonModule
+];
+
+@NgModule({
+  imports: [
+    MaterialComponents
+  ],
+  exports: [
+    MaterialComponents
+  ]
+})
+export class MaterialModule { }
