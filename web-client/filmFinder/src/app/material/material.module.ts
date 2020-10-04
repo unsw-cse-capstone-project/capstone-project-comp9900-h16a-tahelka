731a58287f4a5c35b5157764ecff6c411940847f
Jatin Gupta <guptajatin@outlook.com>
2020-10-04 15:17:25 +1100

    buttons created

--- web-client/filmFinder/src/app/material/material.module.ts
+++ web-client/filmFinder/src/app/material/material.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 const MaterialComponents = [
-  MatButtonModule
+  MatButtonModule,
+  MatIconModule,
+  MatToolbarModule,
 ];
 
 @NgModule({
