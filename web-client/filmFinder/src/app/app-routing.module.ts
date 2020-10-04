b3b5c7c825e10c94692addbff83f804b136a1beb
Jatin Gupta <guptajatin@outlook.com>
2020-10-04 19:36:01 +1100

    routes

--- web-client/filmFinder/src/app/app-routing.module.ts
+++ web-client/filmFinder/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {SignupComponent} from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:  'signup', component:  SignupComponent},
+  { path:  '**', component:  LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- web-client/filmFinder/src/app/navbar/navbar.component.ts
+++ web-client/filmFinder/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  @Input() isLoggedIn: boolean;
   constructor() { }
 
   ngOnInit(): void {
