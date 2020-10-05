c64976192b35c9ed6cd476b28c24993955f93f4c
Yash Tamakuwala <neel64tamakuwala@gmail.com>
2020-10-05 11:59:12 +1100

    [TAH-82]: Changed database-name.

--- server/db_engine.py
+++ server/db_engine.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-Engine = create_engine('sqlite:///filmfinder.db', echo=True)
+Engine = create_engine('sqlite:///filmfinder_tahelka.db', echo=True)
 Session = sessionmaker(bind=Engine)
