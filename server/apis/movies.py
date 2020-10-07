2f5f28a129e74f4b114f6ae22214b1ea496261f1
Ian Dunkerley <i.dunkerley@student.unsw.edu.au>
2020-10-07 19:57:17 +1100

    Updated search

--- server/apis/movies.py
+++ server/apis/movies.py
@@ -3,31 +3,43 @@ from flask import request
 from flask_restx import Namespace, fields, Resource
 from authentication.token_authenticator import TokenAuthenticator
 from models.Movie import Movie
+from models.User import User
+from werkzeug.exceptions import NotFound
 from string import punctuation
 
 
-api = Namespace('Search', path = '/movies', description = 'Search for movies')
+api = Namespace('Movies', 'Find movies', '/movies')
 
-def transform(s):
-    return set(s.lower().translate(str.maketrans('', '', punctuation)).split())
+# def transform(s):
+#     return set(s.lower().translate(str.maketrans('', '', punctuation)).split())
 
 @api.route('')
-class Movies(Resource):
-    @api.param('name', description = 'Name search keywords')
+@api.param('name', 'Name search keywords')
+class MovieSearch(Resource):
     def get(self):
         '''
         Search for movies by name.
         '''
-        TokenAuthenticator(request.headers.get('Authorization'),
-                           False
-                          ).authenticate()
-        name_keywords = transform(request.args.get('name'))
-        movies = Session().query(Movie.movieID, Movie.title,
-                                 Movie.year, Movie.avg_rating
-                                )
-        search_results = [{'movieID': movieID, 'title': title,
-                           'year': year, 'rating': avg_rating
-                          } for movieID, title, year, avg_rating in movies
-                                if name_keywords <= transform(title)
-                         ]
-        return search_results, 200
+        TokenAuthenticator(request.headers.get('Authorization')).authenticate()
+        name_keywords = request.args.get('name') if 'name' in request.args else ''
+        matches = Session().query(Movie.movieID, Movie.title,
+                                  Movie.year, Movie.avg_rating
+                                 ).filter(Movie.title.ilike(f"%{name_keywords}%"))
+        return [{'movieID': movieID, 'title': title,
+                 'year': year, 'rating': avg_rating
+                } for movieID, title, year, avg_rating in matches
+               ], 200
+
+@api.route('/<int:movieID>')
+class MovieDetails(Resource):
+    def get(self, movieID):
+        '''
+        View a movie's full details.
+        '''
+        TokenAuthenticator(request.headers.get('Authorization')).authenticate()
+        session = Session()
+        movie = session.query(Movie).filter(Movie.movieID == movieID).one_or_none()
+        if movie is None:
+            raise NotFound
+        full_details = {}
+        return full_details, 200
