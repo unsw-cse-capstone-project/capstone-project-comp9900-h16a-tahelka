5309769df78170445cef40f4805e26fe8b0d67fb
Nishnat Chokkarapu <nishantchokkarapu@gmail.com>
2020-10-08 20:49:02 +1100

    removed the testGround component

--- web-client/filmFinder/src/app/app-routing.module.ts
+++ web-client/filmFinder/src/app/app-routing.module.ts
@@ -4,14 +4,8 @@ import {LoginComponent} from './login/login.component';
 import {SignupComponent} from './signup/signup.component';
 import {SearchComponent} from './search/search.component';
 import {AuthGuard} from './_helpers/auth.guard';
-import {TestGroundComponent} from './test-ground/test-ground.component';
 
 const routes: Routes = [
-  {
-    path:  'test',
-    component: TestGroundComponent,
-    canActivate: [AuthGuard]
-  },
   {
     path:  'search',
     component:  SearchComponent,
