176733176ec19dddb967ee085e1403aa69e88b75
Nishnat Chokkarapu <nishantchokkarapu@gmail.com>
2020-10-09 05:04:55 +1100

    review component with Forms

--- web-client/filmFinder/src/app/review/review.component.ts
+++ web-client/filmFinder/src/app/review/review.component.ts
@@ -1,4 +1,5 @@
 import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-review',
@@ -9,6 +10,10 @@ export class ReviewComponent implements OnInit {
 
 
   public selectedRating: number;
+  reviewForm = new FormGroup({
+    givenRating: new FormControl(''),
+    givenReview: new FormControl('')
+  });
   @Output() private ratingNew = new EventEmitter();
 
   constructor() { }
@@ -19,6 +24,6 @@ export class ReviewComponent implements OnInit {
     console.log('Review Saved');
   }
   cancelClick(): void {
-    console.log('Review Canceled')
+    console.log('Review Canceled');
   }
 }
