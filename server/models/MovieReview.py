b5c21edb4044a53e6b5443de0a3c758b631f56f0
Ian Dunkerley <i.dunkerley@student.unsw.edu.au>
2020-10-10 16:10:34 +1100

    Updates to database schema

--- server/models/MovieReview.py
+++ server/models/MovieReview.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, Float, String, ForeignKey
 from models.Common import Base
+from time import time
 
 
 class MovieReview(Base):
@@ -7,9 +8,13 @@ class MovieReview(Base):
 
     movieID = Column(Integer, ForeignKey("movies.movieID"), primary_key=True, index=True)
     userID = Column(Integer, ForeignKey("users.userID"), primary_key=True, index=True)
-    rating = Column(Integer, index=True)
+    rating = Column(Float, index=True)
     review = Column(String)
+    timestemp = Column(Float)
 
-    def __init__(self, movieID, userID):
+    def __init__(self, movieID, userID, rating, review):
         self.movieID = movieID
         self.userID = userID
+        self.rating = rating
+        self.review = review
+        self.timestamp = time()
