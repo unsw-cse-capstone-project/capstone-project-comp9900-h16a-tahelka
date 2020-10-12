0b2ef455533d4985a50f2b9fc6bb50f34276b84d
Ian Dunkerley <i.dunkerley@student.unsw.edu.au>
2020-10-12 23:16:18 +1100

    Added genre and description to search functionality

--- server/apis/movie_search.py
+++ server/apis/movie_search.py
@@ -2,6 +2,8 @@ from flask import request
 from flask_restx import Namespace, fields, Resource
 from authentication.token_authenticator import TokenAuthenticator
 from db_engine import Session
+from models.Genres import Genres
+from models.GenreOfFilm import GenreOfFilm
 from models.Movie import Movie
 
 
@@ -16,7 +18,9 @@ film_summary = api.model('Film Summary',
                         )
 
 @api.route('')
-@api.param('name', 'Name search keywords')
+@api.param('name', 'Name keywords')
+@api.param('genre', 'Genre keywords')
+@api.param('description', 'Description keywords')
 class MovieSearch(Resource):
     @api.response(200, 'Success', [film_summary])
     def get(self):
@@ -26,9 +30,22 @@ class MovieSearch(Resource):
         TokenAuthenticator(request.headers.get('Authorization')).authenticate()
         limit = 100  # TODO: change limit later as needed.
         name_keywords = request.args.get('name') if 'name' in request.args else ''
-        search_results = Session().query(Movie.movieID, Movie.title, Movie.year,
-                                         Movie.ratings_sum, Movie.review_count
-                                        ).filter(Movie.title.ilike(f'%{name_keywords}%'))[: limit]
+        description_keywords\
+            = request.args.get('description') if 'description' in request.args else ''
+        search_results\
+            = Session().query(Movie.movieID, Movie.title, Movie.year,
+                              Movie.ratings_sum, Movie.review_count
+                             ).join(GenreOfFilm).join(Genres)\
+                              .filter(Genres.genre == request.args.get('genre'),
+                                      Movie.title.ilike(f'%{name_keywords}%'),
+                                      Movie.description.ilike(f'%{description_keywords}%')
+                                     )[: limit]\
+            if 'genre' in request.args\
+            else Session().query(Movie.movieID, Movie.title, Movie.year,
+                                 Movie.ratings_sum, Movie.review_count
+                                ).filter(Movie.title.ilike(f'%{name_keywords}%'),
+                                         Movie.description.ilike(f'%{description_keywords}%')
+                                        )[: limit]
         return [{'movieID': movieID, 'title': title, 'year': year,
                  'rating': ratings_sum / review_count if review_count else 0
                 } for movieID, title, year, ratings_sum, review_count in search_results
