ab7caef3001e12581adcb09af679529b92363d87
Yash Tamakuwala <neel64tamakuwala@gmail.com>
2020-10-20 23:53:17 +1100

    Enforce Foreign Key Constraints.

--- server/db_engine.py
+++ server/db_engine.py
@@ -1,5 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import event
 
 Engine = create_engine('sqlite:///filmfinder_tahelka.db', echo=True)
 Session = sessionmaker(bind=Engine)
+
+# To enforce Foreign Key constraints.
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
