bace4b685af8a5296c73c61d6736a9d1f69a22bb
Yash Tamakuwala <neel64tamakuwala@gmail.com>
2020-10-27 00:53:16 +1100

    Added db validations for User table and example for email.

--- server/models/User.py
+++ server/models/User.py
@@ -6,8 +6,8 @@ class User(Base):
     __tablename__ = "users"
 
     userID = Column(Integer, primary_key=True)
-    username = Column(String, nullable=False)
-    email = Column(String, unique=True)
+    username = Column(String, nullable=False, unique=True)
+    email = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
     yob = Column(Integer)
 
