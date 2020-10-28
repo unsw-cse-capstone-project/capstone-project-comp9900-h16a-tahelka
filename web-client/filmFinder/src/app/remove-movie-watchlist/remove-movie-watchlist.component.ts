ccd7d5a411d6b06ec473a4c25116af6482587c26
Jatin Gupta <guptajatin@outlook.com>
2020-10-28 11:17:02 +1100

    TAH-34: Remove button and show watched movies

--- web-client/filmFinder/src/app/remove-movie-watchlist/remove-movie-watchlist.component.ts
+++ web-client/filmFinder/src/app/remove-movie-watchlist/remove-movie-watchlist.component.ts
@@ -1,7 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {UserMessageConstant} from '../constants/UserMessageConstant';
 import {WebService} from '../services/web.service';
 import {MatSnackBar} from '@angular/material/snack-bar';
+import {WishlistRemove} from '../models/WishlistRemove';
 
 @Component({
   selector: 'app-remove-movie-watchlist',
@@ -11,6 +12,7 @@ import {MatSnackBar} from '@angular/material/snack-bar';
 export class RemoveMovieWatchlistComponent implements OnInit {
   @Input() public movieID: number;
   snackbarDuration = 2000;
+  @Output() deleteFromWatchlist = new EventEmitter<number>();
   constructor(private webService: WebService, private snackbar: MatSnackBar) { }
 
   ngOnInit(): void {
@@ -18,6 +20,7 @@ export class RemoveMovieWatchlistComponent implements OnInit {
   onClick(): void{
     this.webService.removeWatchlist(this.movieID).subscribe(success => {
       this.successfulUpdateSnackbar(UserMessageConstant.WATCHLIST_REMOVED, UserMessageConstant.DISMISS);
+      this.deleteFromWatchlist.emit(this.movieID);
     }, err => {
       this.successfulUpdateSnackbar(UserMessageConstant.WATCHLIST_REMOVED_UNSUCCESSFUL, UserMessageConstant.DISMISS);
     });
