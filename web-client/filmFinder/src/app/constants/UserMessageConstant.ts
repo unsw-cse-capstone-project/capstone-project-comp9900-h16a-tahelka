39e691298f861d02a11271989bec459ac1ad2321
Jatin Gupta <guptajatin@outlook.com>
2020-10-28 14:21:00 +1100

    limit on search result

--- web-client/filmFinder/src/app/constants/UserMessageConstant.ts
+++ web-client/filmFinder/src/app/constants/UserMessageConstant.ts
@@ -6,8 +6,8 @@ export class UserMessageConstant {
   public static get UNSUBSCRIBED_USER(): string { return 'Unsubscribed to user'; }
   public static get UNSUBSCRIBED_USER_UNSUCCESSFUL(): string { return 'Could not unsubscribed to user, either already unsubscribed or try again!'; }
   public static get WISHLIST_ADDED(): string { return 'Added to wishlist'; }
-  public static get BLOCKED_USER_SUCCESSFUL(): string { return 'User has been blocked'; }
-  public static get BLOCKED_USER_UNSUCCESSFUL(): string { return 'User block was not successful, try again!'; }
+  public static get BLOCKED_USER_SUCCESSFUL(): string { return 'User has been banned'; }
+  public static get BLOCKED_USER_UNSUCCESSFUL(): string { return 'User ban was not successful, try again!'; }
   public static get WISHLIST_REMOVED(): string { return 'Removed from wishlist'; }
   public static get WATCH_LIST_ADDED(): string { return 'Added to watchlist'; }
   public static get WATCH_LIST_ADD_UNSUCCESSFUL(): string { return 'Add to watchlist was not successful, already added or try again!'; }
@@ -19,9 +19,6 @@ export class UserMessageConstant {
   public static get WISHLIST_REMOVE_UNSUCCESSFUL(): string { return 'Could not remove from wishlist, movie is not present or try again!'; }
   public static get WISHLIST_IMPORT_SUCCESSFUL(): string { return 'Wishlist import successful.'; }
   public static get WISHLIST_IMPORT_UNSUCCESSFUL(): string { return 'Wishlist import unsuccessful, already imported or try again!'; }
-  public static get WATCHLIST_REMOVE_UNSUCCESSFUL(): string {
-    return 'Could not remove from watchlist, movie is not present or try again!';
-  }
   public static get DISMISS(): string { return 'Dismiss'; }
 
 }
