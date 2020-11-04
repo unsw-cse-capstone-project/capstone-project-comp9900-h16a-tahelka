c2815a85ba5cb90b241c866102db0137159d6443
Yash Tamakuwala <neel64tamakuwala@gmail.com>
2020-11-04 20:31:31 +1100

    minor changes to recommendation calculation

--- server/RecSystem/rec_calc.py
+++ server/RecSystem/rec_calc.py
@@ -1,19 +1,20 @@
 import pandas as pd 
 import numpy as np 
-import time 
+# import time
 #12.0 seconds (including reading the 4 files)
 
-start = time.time()
-def movie_similarity_calc(movie, director, genre, user, movieID, userID, use_genre, use_director, use_movie = 5):
+# start = time.time()
+def movie_similarity_calc(movie, director, genre, user, movieID, userID, use_genre, use_director, use_movie = 5, use_user = 1):
     #retrieve the rows and concatenate
     movie_row = movie[movie.movieID == movieID]               
     dir_row = director[director.movieID == movieID]
     genre_row = genre[genre.movieID == movieID]
     user_row = user[user.index == userID]
-    final = pd.concat([movie_row, dir_row, genre_row])
+    final = pd.concat([movie_row, dir_row, genre_row, user_row])
+    totalMovies = len(movie_row.columns)
 
     #create weight matrix
-    weights = (np.matrix([[use_movie,use_director,use_genre]]*3878)).T
+    weights = (np.matrix([[use_movie,use_director,use_genre,use_user]]*totalMovies)).T
 
     #calculation
     final = final*weights
@@ -21,8 +22,11 @@ def movie_similarity_calc(movie, director, genre, user, movieID, userID, use_gen
     sim_score.columns = ['score']                     #rename 
     sim_score = sim_score.drop('movieID')             #delete movieID 
     sim_score = sim_score.drop(str(movieID))          #delete actual movieID score
-    sim_score = sim_score.sort_values(by = 'score', ascending = False) #sort 
-    return list(sim_score.head(10).index)                      #return 10 best movies sim_score
+    sim_score = sim_score.sort_values(by = 'score', ascending = False) #sort
+
+    limit = 10
+    topMovies = [int(movie) for movie in list(sim_score.head(limit).index)]
+    return topMovies                    #return 10 best movies sim_score
 
 #SYSTEM INPUT -----------
 movieID = 114709.0
@@ -42,4 +46,4 @@ genre.columns = movie.columns.values
 
 #run file
 movie_similarity_calc(movie, director, genre, user, movieID, userID, use_genre, use_director)
-print(time.time() - start)
+# print(time.time() - start)
